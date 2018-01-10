085075037b7eadf544b0021a7d7498df519111d6
IsaacME <isaac.maya.estrada@gmail.com>
2018-01-10 11:47:55 -0600

    La aplicación ToDoList esta completa con los métodos "delegate" y "datasource" del tableViewController

---
+++ TODOList/TodoListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  TODOList
+//
+//  Created by Luis Isaac Maya Estrada on 10/01/18.
+//  Copyright © 2018 Isaac. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    
+    let itemArray = ["Find Mike", "buy Eggs", "Play Videogames"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+/// MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+    
+    tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+
+
+}
+
